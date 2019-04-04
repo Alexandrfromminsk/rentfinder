fe10f39472114509b28252c7577e6cf5b9793c70
alex <alexandrfromminsk@gmail.com>
2019-04-04 12:57:30 -0400

    Model, first edition

--- appartlist/models.py
+++ appartlist/models.py
@@ -1,8 +1,31 @@
 from django.db import models
+from model_utils import Choices
 
 class Apt(models.Model):
     name=models.CharField(max_length=200)
-    description=models.TextField
-    price=models.IntegerField
-    deposit=models.IntegerField
+    description=models.TextField()
+    price=models.IntegerField()
+    deposit=models.IntegerField()
+    photo_link = models.CharField(max_length=700)
+    bedrooms=models.FloatField(default=1)
+    bathrooms=models.FloatField(default=1)
+    # TYPE_CHOICES=(
+    #     ('PRIVATE', "Private"),
+    #     ('APPART_COMPLEX', "Appartment complex"),
+    # )
+    TYPE_CHOICES = Choices('Private', 'Appartment_complex')
+    type=models.CharField(choices=TYPE_CHOICES, default=TYPE_CHOICES.Private)
+    pool=models.BooleanField(default=False)
+    gym=models.BooleanField(default=False)
+    renters_website = models.CharField(max_length=200)
+    sorce_link = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
+    is_visited = models.BooleanField(default=False)
+    move_in_date = models.DateField()
+    appointment_date=models.DateTimeField()
+    comment=models.TextField()
+    phone=models.CharField(max_length=50)
+    date_added=models.DateTimeField(auto_now_add=True)
+
+
 
